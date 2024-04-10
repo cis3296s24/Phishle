0747bc9d9dbe07944a87b9e2f24ab8c5a95d5ecf
stan39049 <stan39049@gmail.com>
2024-04-10 02:52:12 -0400

    Made page register and save users for the current session
    Saves the users registered into a map(will change when database stuff is figured out). Also, login now checks for the saved user info instead.

--- phishle/client/src/js/login.js
+++ phishle/client/src/js/login.js
@@ -5,6 +5,7 @@ const loginPassword = document.getElementById("loginPassword");
 const regUsername = document.getElementById("regUsername");
 const regPassword = document.getElementById("regPassword");
 const regPassword2 = document.getElementById("regPassword2");
+const loginInformation = new Map();
 
 login.addEventListener("submit", e => {
     e.preventDefault();
@@ -35,15 +36,22 @@ function setSuccess(element){
 function validateLogin(){
     const username = loginUsername.value.trim();
     const password = loginPassword.value.trim();
-    if(username === ""){
-        setError(loginUsername, "Username cannot be empty");
-    }else{
+    userValid = false;
+    passValid = false;
+    if(loginInformation.has(username)){
         setSuccess(loginUsername);
-    }
-    if(password.length > 7){
-        setSuccess(loginPassword);
+        userValid = true;
     }else{
-        setError(loginPassword, "Password must be at least 8 characters long");
+        setError(loginUsername, "Username is not valid");
+    }
+    if(loginInformation.get(username) == password){
+        setSuccess(loginPassword);
+        passValid = true;
+    }else{
+        setError(loginPassword, "Password is incorrect");
+    }
+    if(userValid && passValid){
+        alert("logged in as " + username);
     }
 }
 
@@ -51,19 +59,29 @@ function validateRegister(){
     const username = regUsername.value.trim();
     const password1 = regPassword.value.trim();
     const password2 = regPassword2.value.trim();
+    userValid = false;
+    pass1Valid = false;
+    pass2Valid = false;
     if(username === ""){
         setError(regUsername, "Username cannot be empty");
     }else{
         setSuccess(regUsername);
+        userValid = true;
     }
     if(password1.length > 7){
         setSuccess(regPassword);
+        pass1Valid = true;
     }else{
         setError(regPassword, "Password must be at least 8 characters long");
     }
     if(password2 == password1){
         setSuccess(regPassword2);
+        pass2Valid = true;
     }else{
         setError(regPassword2, "Password must match original password");
+    }
+    if(userValid && pass1Valid && pass2Valid){
+        loginInformation.set(username, password1);
+        alert("Registerd user: " + username);
     }
 }
